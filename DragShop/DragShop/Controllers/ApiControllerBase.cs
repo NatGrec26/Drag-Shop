5f499838a3a43183a02445db070660cbf6728548
Jhonny Rodríguez Paniagua <jhonnyrp18@hotmail.com>
2019-06-18 14:41:09 -0600

    pull
    update

---
+++ DragShop/DragShop/Controllers/ApiControllerBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DragShop.Dtos;
+using DragShop.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DragShop.Controllers
+{
+    [ApiController]
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        public ObjectResult ResolveErrorStatusCode(ErrorDto error)
+        {
+            if (!int.TryParse(error.StatusCode, out int statusCode))
+            {
+                return this.StatusCode(500, this.CreateErrorMessageModel(error));
+            }
+            switch (statusCode)
+            {
+                case 404:
+                    return this.StatusCode(statusCode, this.CreateErrorMessageModel(error));
+                case 400:
+                    return this.BadRequest(this.CreateErrorMessageModel(error));
+                default:
+                    return this.StatusCode(statusCode, this.CreateErrorMessageModel(error));
+            }
+        }
+
+        protected ErrorMessageModel CreateErrorMessageModel(ErrorDto error)
+            => this.CreateErrorMessageModel(error.ErrorCode, error.ErrorMessage);
+
+        protected ErrorMessageModel CreateErrorMessageModel(string errorType, object errorMesage)
+            => new ErrorMessageModel
+            {
+                ErrorType = errorType,
+                ErrorMessage = errorMesage
+            };
+    }
+}
---
+++ DragShop/DragShop/Models/ProductResponseModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragShop.Models
+{
+    public class ProductResponseModel
+    {
+        public int ProductID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int LastPrice { get; set; }
+
+        public string State { get; set; }
+
+        public string Tutorial { get; set; }
+    }
+}
