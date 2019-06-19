bbbb084a4535bb8834fa4c682b9913f73634049b
Natalia <roman.nati26@gmail.com>
2019-06-19 16:28:41 -0600

    send email contact

---
+++ DragShop/DragShop/Models/SendEmailCustomer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragShop.Models
+{
+    public class SendEmailCustomer
+    {
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
