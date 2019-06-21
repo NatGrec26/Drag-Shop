5927d62465f51cf2a1acbea03c8600b5d68ae0ab
Jhonny Rodríguez Paniagua <jhonnyrp18@hotmail.com>
2019-06-21 15:23:26 -0600

    update
    update

--- DragShop/DragShop/ClientApp/src/app/checkout/services/checkout-form.component.ts
+++ DragShop/DragShop/ClientApp/src/app/checkout/services/checkout-form.component.ts
@@ -15,7 +15,11 @@ export class CheckoutFormService extends FormServiceBase {
             firstName: this.fb.control('', [Validators.maxLength(50), Validators.required]),
             lastName: this.fb.control('', [Validators.maxLength(50), Validators.required]),
             email: this.fb.control('', [Validators.required, Validators.email, Validators.maxLength(100)]),
-            message: this.fb.control('', [Validators.required, Validators.maxLength(400)])
+            message: this.fb.control('', [Validators.required, Validators.maxLength(400)]),
+            cardNumber: this.fb.control('', [Validators.required/*, Validators.pattern('[0-9]{4}-[0-9]{4}')*/
+                , Validators.maxLength(19), Validators.minLength(19)]),
+            cvv: this.fb.control('', [Validators.required,
+                Validators.pattern('[0-9]{3}'), Validators.maxLength(3), Validators.minLength(3)]),
         }));
     }
 }
