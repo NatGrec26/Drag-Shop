e8f364eab17f73acdfda31e064bf84cc45f9f504
Jhonny Rodríguez Paniagua <jhonnyrp18@hotmail.com>
2019-06-21 21:32:48 -0600

    update
    update

--- DragShop/DragShop/ClientApp/src/app/checkout/services/checkout-form.component.ts
+++ DragShop/DragShop/ClientApp/src/app/checkout/services/checkout-form.component.ts
@@ -9,17 +9,18 @@ export class CheckoutFormService extends FormServiceBase {
     constructor(fb: FormBuilder) {
         super(fb);
     }
-
     initForm(): FormGroup {
         return (this.form = this.fb.group({
             firstName: this.fb.control('', [Validators.maxLength(50), Validators.required]),
             lastName: this.fb.control('', [Validators.maxLength(50), Validators.required]),
-            email: this.fb.control('', [Validators.required, Validators.email, Validators.maxLength(100)]),
+            email: this.fb.control('', [Validators.required
+                , Validators.pattern('^[^@]+@[^@]+\.[a-zA-Z]{2,}$'),
+                Validators.maxLength(100)]),
             message: this.fb.control('', [Validators.required, Validators.maxLength(400)]),
             cardNumber: this.fb.control('', [Validators.required, Validators.pattern('[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}')
            , Validators.maxLength(19), Validators.minLength(19)]),
-        //    cvv: this.fb.control('', [Validators.required,
-      //          Validators.pattern('[0-9]'), Validators.maxLength(3), Validators.minLength(3)]),
+            cvv: this.fb.control('', [Validators.required,
+            Validators.pattern('[0-9]{3}'), Validators.maxLength(3), Validators.minLength(3)]),
         }));
     }
 }
